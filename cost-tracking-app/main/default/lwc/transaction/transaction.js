99dea93304023644db50151fb891de3143e6f43b
Waldemar Ewert <waldi.ewert@googlemail.com>
2022-02-05 00:10:52 +0100

    setup cost tracking app

---
+++ cost-tracking-app/main/default/lwc/transaction/transaction.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class Transaction extends LightningElement {}
---
+++ cost-tracking-app/main/default/lwc/transactionExtend/transactionExtend.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class TransactionExtend extends LightningElement {}
